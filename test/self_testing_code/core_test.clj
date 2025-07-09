92da18b49998512a629ce7fcf04d235320f67c1f
Maia <gustavomaia334@gmail.com>
2025-07-09 11:16:34 -0300

    first commit

---
+++ test/self_testing_code/core_test.clj
@@ -0,0 +1,11 @@
+(ns self-testing-code.core-test
+  (:require [clojure.test :refer :all]
+            [self-testing-code.core :refer :all]))
+
+(deftest nao-sei-o-nome-ainda-test
+  (testing "Dado um valor abaixo de 100 reais, então a taxa de entrega deve ser de 15 reais"
+    (is (= 15 (taxa-de-entrega 1)))
+    (is (= 15 (taxa-de-entrega 100))))
+  (testing "Se for maior que 100 e menor que 200, 5 reais"
+    (is (= 5 (taxa-de-entrega 100.01)))
+    (is (= 5 (taxa-de-entrega 200)))))
