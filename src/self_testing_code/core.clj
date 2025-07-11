86ecc8ced037d6499b1579945f9e8775dbef2c0b
Maia <gustavomaia334@gmail.com>
2025-07-11 11:07:36 -0300

    pop function tested

--- src/self_testing_code/core.clj
+++ src/self_testing_code/core.clj
@@ -1,14 +1,9 @@
 (ns self-testing-code.core)
-(defn taxa-de-entrega
-"Se valor da compra for até 100, deve-se cobrar 15 reais de taxa de entrega.
-Se for maior que 100 e menor que 200, 5 reais
-Se for acima de 200, a taxa é gratuita"
-  [valor-da-compra]
-  (let[taxa-abaixo-de-100 15
-       taxa-acima-de-100 5
-       taxa-acima-de-200 0]
-  (if (<= valor-da-compra 100)
-    taxa-abaixo-de-100
-    (if (<= valor-da-compra 200)
-      taxa-acima-de-100
-      taxa-acima-de-200 ))))
+
+(pop ["Batata" "Arroz" "Feijão"])
+
+(defn desistir-ultimo-item-da-compra
+  [compras]
+  (pop compras))
+
+(desistir-ultimo-item-da-compra ["Arroz" "Feijão" "Batata"])
