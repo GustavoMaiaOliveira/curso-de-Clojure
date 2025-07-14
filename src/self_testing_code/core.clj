e2142f6bef613871c9c7ed9429a04d6ad746c103
Maia <gustavomaia334@gmail.com>
2025-07-14 16:00:30 -0300

    module 9

--- src/self_testing_code/core.clj
+++ src/self_testing_code/core.clj
@@ -1,9 +1,45 @@
 (ns self-testing-code.core)
 
-(pop ["Batata" "Arroz" "Feijão"])
+(conj nil "Banana")
 
-(defn desistir-ultimo-item-da-compra
-  [compras]
-  (pop compras))
+(conj () "Banana")
 
-(desistir-ultimo-item-da-compra ["Arroz" "Feijão" "Batata"])
+(conj(conj nil "Banana")"Arroz")
+
+(conj [1 2 3] nil)
+
+(pop ["Banana"])
+
+;(pop [])
+
+(pop nil)
+
+(defn imprime-primeiro-nome
+  [nomes]
+  (println (get nomes 0 "Not found")))
+
+(imprime-primeiro-nome
+  ["Gustavo" "Maia" "Oliveira"])
+
+(defn imprime-segundo-nome
+  [nomes]
+  (println (nomes 1)))
+
+(imprime-segundo-nome
+  ["Gustavo" "Maia" "Oliveira"])
+
+(defn imprime-terceiro-nome
+  [nomes]
+  (println (nomes 2)))
+
+(imprime-terceiro-nome
+  ["Gustavo" "Maia" "Oliveira"])
+
+(imprime-primeiro-nome
+  [])
+
+(defn substituir-primeiro-nome
+  [nome-velho nome-novo]
+  (assoc nome-velho 0 nome-novo))
+
+(substituir-primeiro-nome ["marcio" "joao" "maria"] "jose")
