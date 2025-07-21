15f751a6df3a8934edc976562460a2aa0496278f
Maia <gustavomaia334@gmail.com>
2025-07-21 11:01:50 -0300

    module 10 finished

--- src/self_testing_code/core.clj
+++ src/self_testing_code/core.clj
@@ -1,45 +1,69 @@
 (ns self-testing-code.core)
 
-(conj nil "Banana")
+;(conj nil "Banana")
+;
+;(conj () "Banana")
+;
+;(conj(conj nil "Banana")"Arroz")
+;
+;(conj [1 2 3] nil)
+;
+;(pop ["Banana"])
+;
+;;(pop [])
+;
+;(pop nil)
+;
+;(defn imprime-primeiro-nome
+;  [nomes]
+;  (println (get nomes 0 "Not found")))
+;
+;(imprime-primeiro-nome
+;  ["Gustavo" "Maia" "Oliveira"])
+;
+;(defn imprime-segundo-nome
+;  [nomes]
+;  (println (nomes 1)))
+;
+;(imprime-segundo-nome
+;  ["Gustavo" "Maia" "Oliveira"])
+;
+;(defn imprime-terceiro-nome
+;  [nomes]
+;  (println (nomes 2)))
+;
+;(imprime-terceiro-nome
+;  ["Gustavo" "Maia" "Oliveira"])
+;
+;(imprime-primeiro-nome
+;  [])
+;
+;(defn substituir-primeiro-nome
+;  [nome-velho nome-novo]
+;  (assoc nome-velho 0 nome-novo))
+;
+;(substituir-primeiro-nome ["marcio" "joao" "maria"] "jose")
 
-(conj () "Banana")
+; tipos
 
-(conj(conj nil "Banana")"Arroz")
+;(defn soma
+;  [x y]
+;  (+ x y))
+;
+;(defn imprime-soma
+;  []
+;  (println(soma nil 2)))
+;
+;(imprime-soma)
 
-(conj [1 2 3] nil)
-
-(pop ["Banana"])
-
-;(pop [])
-
-(pop nil)
-
-(defn imprime-primeiro-nome
-  [nomes]
-  (println (get nomes 0 "Not found")))
-
-(imprime-primeiro-nome
-  ["Gustavo" "Maia" "Oliveira"])
-
-(defn imprime-segundo-nome
-  [nomes]
-  (println (nomes 1)))
-
-(imprime-segundo-nome
-  ["Gustavo" "Maia" "Oliveira"])
-
-(defn imprime-terceiro-nome
-  [nomes]
-  (println (nomes 2)))
-
-(imprime-terceiro-nome
-  ["Gustavo" "Maia" "Oliveira"])
-
-(imprime-primeiro-nome
-  [])
-
-(defn substituir-primeiro-nome
-  [nome-velho nome-novo]
-  (assoc nome-velho 0 nome-novo))
-
-(substituir-primeiro-nome ["marcio" "joao" "maria"] "jose")
+(type 10) ; type long
+(type 10.0) ; type double
+(type(/ 10 3)) ; type ratio
+(type (* 3 (/ 10 3))) ; type BigInt
+(type 10N) ; type BigInt
+(type 10M) ; type BigDecimal
+(type "Maia") ; type String
+(type nil) ; type nil
+(type [])
+(type [1 2 3])
+(type println)
